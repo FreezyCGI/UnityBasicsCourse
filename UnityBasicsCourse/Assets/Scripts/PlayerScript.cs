f62d7f7a61a667b6c3eb1cd8cee7519a77ef4680
FreezyCGI <patrick.fritzl@gmx.at>
2023-06-21 17:51:17 +0200

    Added Player with movement

---
+++ UnityBasicsCourse/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float movementSpeed = 0.1f;
+    public float mouseXSpeed = 10;
+    public float mouseYSpeed = 10;
+
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalMouseInput = Input.GetAxis("Mouse X");
+        float verticalMouseInput = Input.GetAxis("Mouse Y");
+
+        transform.position += transform.forward * verticalInput * movementSpeed * Time.deltaTime;
+        transform.position += transform.right * horizontalInput * movementSpeed * Time.deltaTime;
+
+        transform.Rotate(0, horizontalMouseInput * mouseXSpeed * Time.deltaTime, 0);
+        mainCamera.transform.Rotate(-verticalMouseInput * mouseYSpeed * Time.deltaTime, 0, 0);
+
+        if(mainCamera.transform.rotation.eulerAngles.x > 80 && mainCamera.transform.rotation.eulerAngles.x < 160)
+        {
+            mainCamera.transform.rotation = Quaternion.Euler(80, mainCamera.transform.rotation.y, mainCamera.transform.rotation.z);
+        }
+        if (mainCamera.transform.rotation.eulerAngles.x >= 160 && mainCamera.transform.rotation.eulerAngles.x < 280)
+        {
+            mainCamera.transform.rotation = Quaternion.Euler(280, mainCamera.transform.rotation.y, mainCamera.transform.rotation.z);
+        }
+    }
+}
